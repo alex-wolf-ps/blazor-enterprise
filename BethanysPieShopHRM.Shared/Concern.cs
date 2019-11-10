2ad83c6505c7826797f20d63be0983ceab3c1b3f
Alex Wolf <alexrudiwolf@gmail.com>
2019-11-09 22:29:35 -0500

    Updates

---
+++ BethanysPieShopHRM.Shared/Concern.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Shared
+{
+    public class Concern
+    {
+        public int ConcernId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BethanysPieShopHRM.Shared/HRTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BethanysPieShopHRM.Shared
+{
+    public class HRTask
+    {
+        public int TaskId { get; set; }
+
+        public string Title { get; set; }
+        
+        public string Description { get; set; }
+        
+        public int AssignedTo { get; set; }
+
+        public Status Status { get; set; }
+
+    }
+
+    public enum Status
+    {
+        Open,
+        Assigned,
+        InProgress,
+        Blocked,
+        Complete
+    }
+}
