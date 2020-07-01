c85d597c57dc9e3738bcfc1f380abe35484c101b
Brian Larkworthy <blarkworthy@uwhealth.org>
2020-07-01 15:19:30 -0500

    Complete module 1

---
+++ BethanysPieShopHRM.UI/Pages/TaskListBase.cs
@@ -0,0 +1,38 @@
+﻿using BethanysPieShopHRM.Shared;
+using BethanysPieShopHRM.UI.Services;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM.UI.Pages
+{
+    public class TaskListBase : ComponentBase
+    {
+        [Inject]
+    public ITaskDataService taskService { get; set; }
+
+    [Inject]
+    public NavigationManager navManager { get; set; }
+
+    [Parameter]
+    public int Count { get; set; }
+
+    public List<HRTask> Tasks { get; set; } = new List<HRTask>();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Tasks = (await taskService.GetAllTasks()).ToList();
+
+        if(Count !=0)
+        {
+            Tasks = Tasks.Take(Count).ToList();
+        }
+    }
+
+    public void AddTask()
+    {
+        navManager.NavigateTo("taskedit");
+    }
+    }
+}
